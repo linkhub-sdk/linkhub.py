547ee7c06b72769202bd46a6f456e8babdb32b1f
김성준 <kimsj@innopost.com>
2015-01-20 16:06:05 +0900

    Initialize Repository.

---
+++ linkhub/__init__.py
@@ -0,0 +1,15 @@
+__version__ = '1.0.0'
+Version = __version__  # for backward compatibility
+__all__ = ["Token","LinkhubException"]
+from .linkhub import Token, LinkhubException
+
+TokenInstance = Token()
+
+def generateToken(LinkID,SecretKey,ServiceID,AccessID,Scope,forwardIP = None):
+	return TokenInstance.get(LinkID,SecretKey,ServiceID,AccessID,Scope,forwardIP)
+
+def getBalance(_Token):
+	return TokenInstance.balance(_Token)
+
+def getPartnerBalance(_Token):
+	return TokenInstance.partnerBalance(_Token)
---
+++ linkhub/setup.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+
+try:
+  from setuptools import setup
+except ImportError:
+  from distutils.core import setup
+
+import sys
+
+from linkhub import __version__
+
+if sys.version_info <= (2, 5):
+	error = "ERROR: linkhub requires Python Version 2.6 or above...exiting."
+	print(error, file=sys.stderr)
+	sys.exit(1)
+
+setup(name = "linkhub",
+	  version = __version__,
+	  description = "Linkhub Auth Library",
+	  long_description = "Linkhub API Authority Library.",
+	  author = "Kim Seongjun",
+	  author_email = "pallet027@gmail.com",
+	  scripts = ["linkhub/linkhub"],
+	  url = "https://github.com/linkhub-sdk/Linkhub.py",
+	  packages = ["linkhub", "linkhub.LinkhubException"],
+	  license = "MIT",
+	  platforms = "Posix; MacOS X; Windows",
+	  classifiers = ["Development Status :: 5 - Production/Stable",
+					 "Intended Audience :: Developers",
+					 "License :: OSI Approved :: MIT License",
+					 "Operating System :: OS Independent",
+					 "Topic :: Internet",
+					 "Programming Language :: Python :: 2",
+					 "Programming Language :: Python :: 2.6",
+					 "Programming Language :: Python :: 2.7",
+					 "Programming Language :: Python :: 3",
+					 "Programming Language :: Python :: 3.3",
+					 "Programming Language :: Python :: 3.4"]
+	  )
